ded243eac2a6cda6e828c252b63bfb083cdeaace
Theo Foucher <theonicolas.foucher@gmail.com>
2026-03-20 22:14:35 +0100

    fix - css

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,19 @@ const webSocketServer = {
   configureServer(server) {
     if (!server.httpServer) return;
     // Dynamically import to support ESM modules with top-level await
-    import("./src/lib/server/socket/game.js").then(({ register }) => {
-      import("./src/lib/server/services/playlist.js").then(
-        ({ preloadAllPlaylists }) => {
-          const io = new Server(server.httpServer, {
-            transports: ["websocket", "polling"],
-            perMessageDeflate: { threshold: 1024 },
-          });
-          register(io);
-          preloadAllPlaylists();
-          console.log("[ZIK] Socket.IO attache au serveur Vite");
-        },
-      );
+    Promise.all([
+      import("./src/lib/server/socket/game.js"),
+      import("./src/lib/server/socket/salon.js"),
+      import("./src/lib/server/services/playlist.js"),
+    ]).then(([{ register }, { registerSalon }, { preloadAllPlaylists }]) => {
+      const io = new Server(server.httpServer, {
+        transports: ["websocket", "polling"],
+        perMessageDeflate: { threshold: 1024 },
+      });
+      register(io);
+      registerSalon(io);
+      preloadAllPlaylists();
+      console.log("[ZIK] Socket.IO attache au serveur Vite");
     });
   },
 };
